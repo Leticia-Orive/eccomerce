7da4bd9b5fa10c569428385a3dab54c95e2e297b
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-08-01 13:47:43 +0200

    withComponentIntpuBinding

--- signal-store/src/app/products/features/product-detail/product-detail.component.ts
+++ signal-store/src/app/products/features/product-detail/product-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject,  } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, effect, input } from '@angular/core';
+
 
 @Component({
   selector: 'app-product-detail',
@@ -9,10 +9,18 @@ import { ActivatedRoute } from '@angular/router';
   styles: ``
 })
 export default class ProductDetailComponent {
+  /*obtener los parametros de la ruta * 
+
   private activatedRoute= inject(ActivatedRoute);
   constructor() {
     this.activatedRoute.params.subscribe(params => {
       console.log(params);
+    });
+  }*/
+  id = input.required<string>();
+  constructor(){
+    effect(() => {
+      console.log(this.id);
     });
   }
 
